87465673c1dfa2cf9581a222744e0e7323bcc4c9
GODSFAVOUR111 <iyayi.zusi@gmail.com>
2022-06-30 14:08:00 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
-nclude<stdio.h>
+#include <stdio.h>
 /**
- *  * main - prints a phrase
- *   *
- *    * Return: 0 at the end of the program
- *     *
- *      *
- *       */
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
 
 }
