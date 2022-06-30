4174c833968c4087b930223bd902dffde97ec543
GODSFAVOUR111 <iyayi.zusi@gmail.com>
2022-06-30 13:33:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 /**
- *  * main - prints a phrase
- *   *
- *    * Return: 0 at the end of the program
- *     *
- *      *
- *       */
+ * main - prints a phrase 
+ * Return: 0 at the end of the program
+ */
 int main(void)
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
+
+}
