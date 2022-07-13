87e7febd1d875496276a1be8bddf2a00cde62e87
GODSFAVOUR111 <iyayi.zusi@gmail.com>
2022-07-13 13:26:27 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- *  * _strlen - Swap the value of two integers
- *   * @s: Set of characters
- *    *
- *     * Return: Nothing
- *     */
+ * _strlen - Swap the value of two integers
+ * @s: Set of characters
+ *    
+ * Return: Nothing
+ */
 
 int _strlen(char *s)
 {
-		int i = 0;
+int i = 0;
 
-			while (*(s + i) != '\0')
-					{
-								i++;
-									}
+while (*(s + i) != '\0')
+{
+i++;
+}
 
-				return (i);
+return (i);
 }
