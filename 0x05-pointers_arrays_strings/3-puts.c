a82c1c1d2f3405eb8c694bb78fa05001b1deb1a6
GODSFAVOUR111 <iyayi.zusi@gmail.com>
2022-07-12 17:44:16 -0700

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,18 @@
+nclude "main.h"
+/**
+ *  * _puts - Print a string
+ *   * @str: Set of characters
+ *    *
+ *     * Return: Nothing
+ *     */
+void _puts(char *str)
+{
+		int i = 0;
 
+			while (*(str + i) != '\0')
+					{
+								_putchar(*(str + i));
+										i++;
+											}
+				_putchar('\n');
+}
