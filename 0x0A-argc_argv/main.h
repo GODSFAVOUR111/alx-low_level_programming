89b757352575e544d56fe53573522d5b0a51e872
GODSFAVOUR111 <iyayi.zusi@gmail.com>
2022-07-23 06:33:56 -0700

    task0

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _putchar(char c);/*prototype _putchar*/
+int _putchar(char c);
 
-#endif /* MAIN_H */
+#endif /* _MAIN_H_ */
