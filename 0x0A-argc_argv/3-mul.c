2fd0f961bd831b80ac75eab864b67a46a2c41ac0
GODSFAVOUR111 <iyayi.zusi@gmail.com>
2022-07-24 11:55:37 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,22 +1,32 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * main - Program that prints the result  that multiplies two numbers.
- * @argc: count
- * @argv: array
- *
- * Return: Always 0
- **/
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
+ */
+
 int main(int argc, char *argv[])
 {
-		(void) argc;
-			if (*(argv + 1) == '\0' || *(argv + 2) == '\0')
+		int i, j;
+
+			if (argc == 1 || argc == 2)
 					{
 								printf("Error\n");
 										return (1);
 											}
 				else
-							printf("%d\n", (atoi(*(argv + 1)) * atoi(*(argv + 2))));
+						{
+									j = 1;
+
+											for (i = 1; i < 3; i++)
+															j *= atoi(argv[i]);
+
+													printf("%d\n", j);
+														}
+
 					return (0);
 }
